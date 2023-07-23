7d6e469016020911372e1621caf5cce61fa273d2
boodigowda <boodigowda73@gmail.com>
2023-07-23 02:45:16 -0700

    grouping the tests using describe method

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,15 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet"
 
-test('Greet Renders correctly', () => {
-    render(<Greet />);
-    const textElement = screen.getByText(/hello/i);
-    expect(textElement).toBeInTheDocument();
+
+
+describe('GREET', () => {
+    test('Renders correctly', () => {
+        render(<Greet />);
+        const textElement = screen.getByText(/hello/i);
+        expect(textElement).toBeInTheDocument();
+    })
+
+    describe('NESTED', () => {
+        test('renders with a name', () => {
+            render(<Greet name="Boodi" />);
+            const textElement = screen.getByText("Hello Boodi");
+            expect(textElement).toBeInTheDocument();
+
+        })
+    })
 })
 
-test('Green renders with a name', () => {
-    render(<Greet name="Boodi" />);
-    const textElement = screen.getByText("Hello Boodi");
-    expect(textElement).toBeInTheDocument();
+describe('NESTED', () => {
+    test('renders with a name', () => {
+        render(<Greet name="Boodi" />);
+        const textElement = screen.getByText("Hello Boodi");
+        expect(textElement).toBeInTheDocument();
 
+    })
 })
