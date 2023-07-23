8223ab9d86b9a1afe3ebc9800739b8af13f90738
boodigowda <boodigowda73@gmail.com>
2023-07-23 06:04:12 -0700

    Naming Conventiond and using IT instead of test

---
+++ src/__tests__/greet-test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "../components/greet/Greet";
+
+
+
+describe('GREET', () => {
+    test('Renders correctly', () => {
+        render(<Greet />);
+        const textElement = screen.getByText(/hello/i);
+        expect(textElement).toBeInTheDocument();
+    })
+
+    describe('NESTED', () => {
+        test('renders with a name', () => {
+            render(<Greet name="Boodi" />);
+            const textElement = screen.getByText("Hello Boodi");
+            expect(textElement).toBeInTheDocument();
+
+        })
+    })
+})
+
+describe('NESTED', () => {
+    test('renders with a name', () => {
+        render(<Greet name="Boodi" />);
+        const textElement = screen.getByText("Hello Boodi");
+        expect(textElement).toBeInTheDocument();
+
+    })
+})
