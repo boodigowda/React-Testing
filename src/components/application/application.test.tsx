a19b1b174f8f2b3519f7beeee604193adc896fb5
boodigowda <boodigowda73@gmail.com>
2023-07-24 08:39:52 -0700

    added the option using second argument for the getByRole method. to find the particular Element

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -4,7 +4,9 @@ import Application from "./Application";
 describe('Application', () => {
     test('component renders correctly', () => {
         render(<Application />);
-        const textElement = screen.getByRole("textbox");
+        const textElement = screen.getByRole("textbox", {
+            name: 'Name'
+        });
         expect(textElement).toBeInTheDocument();
     })
 
@@ -25,6 +27,28 @@ describe('Application', () => {
         const submitButton = screen.getByRole("button");
         expect(submitButton).toBeInTheDocument();
     })
+
+    test('Bio Text field test', () => {
+        render(<Application />);
+        const bioElement = screen.getByRole("textbox", {
+            name: "Bio" //Inner text of the Element
+        });
+        expect(bioElement).toBeInTheDocument();
+    })
+
+    test('Page Heading', () => {
+        render(<Application />);
+        const pageHeading1 = screen.getByRole("heading", {
+            level: 1 //level 1 means h1
+        });
+        expect(pageHeading1).toBeInTheDocument();
+
+        const pageHeading2 = screen.getByRole("heading", {
+            name: "Section 1" // add innertext also
+        });
+        expect(pageHeading2).toBeInTheDocument();
+    })
+
 });
 
 
